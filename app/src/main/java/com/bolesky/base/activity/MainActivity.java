fc84eebc5ba633e61d2410208a18dce65e151b55
BoleSky <hellocui@aliyun.com>
2016-10-17 17:50:34 +0800

    格式化代码

--- app/src/main/java/com/bolesky/base/activity/MainActivity.java
+++ app/src/main/java/com/bolesky/base/activity/MainActivity.java
@@ -57,14 +57,14 @@ public class MainActivity extends BaseActivity {
             R.string.status,
             R.string.group};
     //Tab 选中图片
-    private final int[] TAB_SELECTED_IMGS = new int[]{
+    private final int[] TAB_SELECTED_IMAGES = new int[]{
             R.drawable.ic_tab_home_active,
             R.drawable.ic_tab_subject_active,
             R.drawable.ic_tab_status_active,
             R.drawable.ic_tab_group_active};
 
     //Tab 未选中图片
-    private final int[] TAB_UNSELECTED_IMGS = new int[]{
+    private final int[] TAB_UNSELECTED_IMAGES = new int[]{
             R.drawable.ic_tab_home_normal,
             R.drawable.ic_tab_subject_normal,
             R.drawable.ic_tab_status_normal,
@@ -129,11 +129,15 @@ public class MainActivity extends BaseActivity {
 //                ViewHelper.setAlpha(ssl, 1 - percent);
             }
         });
-        FragmentTabPagerAdapter fragmentTabPagerAdapter = new FragmentTabPagerAdapter(getSupportFragmentManager(), TAB_FRAGMENTS, this);
+        FragmentTabPagerAdapter fragmentTabPagerAdapter = new FragmentTabPagerAdapter(this.getSupportFragmentManager(),
+                TAB_FRAGMENTS, this);
         mViewPager.setAdapter(fragmentTabPagerAdapter);
         initTabNavigator();
     }
 
+    /**
+     * 初始化底部导航TabNavigator
+     */
     private void initTabNavigator() {
 
         TabNavigator tabNavigator = new TabNavigator(this);
@@ -152,7 +156,7 @@ public class MainActivity extends BaseActivity {
                 View view = LayoutInflater.from(context).inflate(R.layout.layout_tab, null);
                 final ImageView image = (ImageView) view.findViewById(R.id.iv_image);
                 final TextView title = (TextView) view.findViewById(R.id.tv_title);
-                image.setImageResource(TAB_UNSELECTED_IMGS[index]);
+                image.setImageResource(TAB_UNSELECTED_IMAGES[index]);
                 title.setText(TAB_TITLES[index]);
                 pagerTitleView.setContentView(view);
 
@@ -172,14 +176,14 @@ public class MainActivity extends BaseActivity {
                     public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
                         image.setScaleX(1.0f + (1.2f - 1.0f) * leavePercent);
                         image.setScaleY(1.0f + (1.2f - 1.0f) * leavePercent);
-                        image.setImageResource(TAB_UNSELECTED_IMGS[index]);
+                        image.setImageResource(TAB_UNSELECTED_IMAGES[index]);
                     }
 
                     @Override
                     public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
                         image.setScaleX(1.0f + (1.5f - 1.0f) * enterPercent);
                         image.setScaleY(1.0f + (1.5f - 1.0f) * enterPercent);
-                        image.setImageResource(TAB_SELECTED_IMGS[index]);
+                        image.setImageResource(TAB_SELECTED_IMAGES[index]);
                     }
                 });
 
