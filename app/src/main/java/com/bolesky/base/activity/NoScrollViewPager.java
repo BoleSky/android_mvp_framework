c7b3a15d4475385a5cdfe859431c5adf895f29e8
BoleSky <hellocui@aliyun.com>
2016-10-17 16:00:21 +0800

    删除activity_tab_layout.xml

---
+++ app/src/main/java/com/bolesky/base/activity/NoScrollViewPager.java
@@ -0,0 +1,40 @@
+package com.bolesky.base.activity;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by xiaoyong.cui
+ * on 2016/10/17
+ * E-Mail:hellocui@aliyun.com
+ */
+
+public class NoScrollViewPager extends ViewPager {
+    //false:禁止滑动
+    //true:可以滑动
+    private boolean isPagingEnabled = false;
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onInterceptTouchEvent(event);
+    }
+
+    public void setPagingEnabled(boolean b) {
+        this.isPagingEnabled = b;
+    }
+}
